7cdb54ed04774e9eb772a504dbee436e1c1cd0a4
agustingom99 <agustingom99@gmail.com>
2022-08-12 09:27:41 -0300

    primer commit

---
+++ SocialMedia/SocialMedia Core/Entities/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace SocialMedia_Core.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+
+
+
+
+    }
+}
---
+++ SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia_Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://miapi.com {x}",
+                UserId = x * 2
+            });
+            return posts;
+        }
+    }
+}
